687a0816344fc718c5c83760524d33ac452acfbc
tipnist <tejas@DESKTOP-7T9MQ69>
2016-03-03 15:00:39 -0600

    This is first jenks through new repo

---
+++ JenkinsProject/src/myjenkins/MyjenkinsFirst.java
@@ -0,0 +1,12 @@
+package myjenkins;
+
+public class MyjenkinsFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is from my jenkis");
+
+	}
+
+}
